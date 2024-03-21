511f01f52c5df992dc6ce93170918b1191d011cc
al3eex <a.zaxapov@yandex.ru>
2024-03-21 03:18:13 +0300

    dl-pl changed

--- dl-pl.sh
+++ dl-pl.sh
@@ -1,1 +1,2 @@
-yt-dlp -o "./%(playlist_title)s/%(playlist_index)s_%(title)s.%(ext)s" "$1"
+# Download playlist into it's own directory
+yt-dlp -o "./%(playlist_title)s/%(playlist_index)s_%(title)s.%(ext)s" $1
